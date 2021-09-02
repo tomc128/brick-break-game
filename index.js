c8ebdf2837d9dfaa6cbb84c955d0ba3fba43b9fc
Tom Chapman <43298139+tomc128@users.noreply.github.com>
2021-09-02 23:01:08 +0100

    Added ball shooting in direction of mouse cursor on click

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let ctx = canvas.getContext('2d');
 
 
 canvas.addEventListener('mousemove', onMouseMove);
+canvas.addEventListener('click', onMouseDown);
 
 
 // Internal
@@ -11,36 +12,55 @@ let lastUpdate = Date.now();
 
 // Ball
 let ballRadius = 10;
+let ballPos = { x: canvas.width / 2, y: canvas.height - ballRadius - 10 };
+let ballSpeed = 100;
 
-let x = canvas.width / 2;
-let y = canvas.height - 30;
-let dx = 1;
-let dy = -1;
+let dy = dx = 0;
 
 
 // Mouse & crosshair
-let mousePosition = { x: 0, y: 0 };
+let mousePos = { x: 0, y: 0 };
 let crosshairThickness = 1;
 let crosshairLength = 8;
 
 
 function onMouseMove(event) {
-
     let rect = canvas.getBoundingClientRect();
 
-    mousePosition = {
+    mousePos = {
         x: event.clientX - rect.left,
         y: event.clientY - rect.top
     };
+}
 
+function onMouseDown(event) {
+    // Fire ball
+    let direction = getBallMovementDirection();
+    
+    dx = direction.x;
+    dy = direction.y;
+}
+
+
+
+function getBallMovementDirection() {
+    // ballpos -> mousepos
+    let x = mousePos.x - ballPos.x;
+    let y = mousePos.y - ballPos.y;
+    let magnitude = Math.sqrt(x*x + y*y);
+
+    return {
+        x: x / magnitude,
+        y: y / magnitude
+    };
 }
 
 
 
 function drawCrosshair() {
     ctx.beginPath();
-    ctx.rect(mousePosition.x - crosshairLength, mousePosition.y - crosshairThickness, crosshairLength * 2, crosshairThickness * 2);
-    ctx.rect(mousePosition.x - crosshairThickness, mousePosition.y - crosshairLength, crosshairThickness * 2, crosshairLength * 2);
+    ctx.rect(mousePos.x - crosshairLength, mousePos.y - crosshairThickness, crosshairLength * 2, crosshairThickness * 2);
+    ctx.rect(mousePos.x - crosshairThickness, mousePos.y - crosshairLength, crosshairThickness * 2, crosshairLength * 2);
     ctx.fillStyle = '#000000aa';
     ctx.fill();
     ctx.closePath();
@@ -48,7 +68,7 @@ function drawCrosshair() {
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.arc(ballPos.x, ballPos.y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = '#32ee68';
     ctx.fill();
     ctx.closePath();
@@ -57,19 +77,20 @@ function drawBall() {
 
 function update(deltaTime) {
     // Ball position & edge collision
-    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) dx *= -1;
-    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) dy *= -1;
+    let dxt = dx * deltaTime;
+    let dyt = dy * deltaTime;
 
-    x += dx * deltaTime;
-    y += dy * deltaTime;
+    if (ballPos.x + dxt > canvas.width - ballRadius || ballPos.x + dxt < ballRadius) dx *= -1;
+    if (ballPos.y + dyt > canvas.height - ballRadius || ballPos.y + dyt < ballRadius) dy *= -1;
 
-    draw();
+    ballPos.x += dx * deltaTime * ballSpeed;
+    ballPos.y += dy * deltaTime * ballSpeed;
 }
 function fixedUpdate() {
 
 }
 
-function draw(deltaTime) {
+function render() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     drawBall();
@@ -82,7 +103,7 @@ function tick() {
     lastUpdate = now;
 
     update(deltaTime);
-    draw(deltaTime);
+    render();
 
     window.requestAnimationFrame(tick);
 }
